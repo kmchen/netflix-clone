b062c72474d4daba55d755a701f58b4dfd7e6fa4
Kuang-Ming Chen <kuangmingchen0702@gmail.com>
2025-03-20 13:39:30 +0100

    Init commit for frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { Route, Routes } from "react-router-dom"
+import Login from "./pages/Login"
+import Signup from "./pages/Signup"
+import Home from "./pages/Home"
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/login" element={<Login/>} ></Route>
+      <Route path="/" element={<Home/>} ></Route>
+      <Route path="/signup" element={<Signup/>} ></Route>
+    </Routes>
+  )
+}
+
+export default App
