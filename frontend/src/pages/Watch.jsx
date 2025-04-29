aebb2365a9d77b0df31fff113b68ad4f687c2504
Kuang-Ming Chen <kuangmingchen0702@gmail.com>
2025-04-30 00:25:42 +0200

    Implement similar movies slider

--- frontend/src/pages/Watch.jsx
+++ frontend/src/pages/Watch.jsx
@@ -1,32 +1,90 @@
-import React, { useEffect } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import Navbar from "./components/Navbar";
 import { ChevronRight, ChevronLeft } from "lucide-react";
 import ReactPlayer from "react-player";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import axios from "axios";
-import { ORIGINAL_IMG_BASE_URL } from "../utils/constants";
+import { SMALL_IMG_BASE_URL, ORIGINAL_IMG_BASE_URL } from "../utils/constants";
 import { formatReleaseDate } from "../utils/dateFunction";
 
 const WatchPage = () => {
   const { id } = useParams();
-  const [details, setDetails] = React.useState();
+  const [details, setDetails] = useState();
+  const [trailers, setTrailers] = useState();
+  const [trailerIndex, setTrailerIndex] = useState(0);
+  const [similarContent, setSimilarContent] = useState();
+  const [showArrows, setShowArrows] = useState(false);
+  const sliderRef = useRef(null);
+  const scrollLeft = () => {
+    if (sliderRef.current) {
+      sliderRef.current.scrollBy({
+        left: -sliderRef.current.offsetWidth,
+        behavior: "smooth",
+      });
+    }
+  };
+  const scrollRight = () => {
+    if (sliderRef.current) {
+      sliderRef.current.scrollBy({
+        left: sliderRef.current.offsetWidth,
+        behavior: "smooth",
+      });
+    }
+  };
+  const onMouseEnter = () => {
+    setShowArrows(true);
+  };
+  const onMouseLeave = () => {
+    setShowArrows(false);
+  };
+  useEffect(() => {
+    const getTrailers = async () => {
+      const res = await axios.get(`/api/v1/movie/${id}/trailers`);
+      setTrailers(res.data.trailers);
+      setTrailerIndex(0);
+    };
+    getTrailers();
+  }, [id]);
   useEffect(() => {
     const getDetails = async () => {
       const res = await axios.get(`/api/v1/movie/${id}/details`);
       setDetails(res.data.details);
     };
     getDetails();
-  }, []);
+  }, [id]);
+  useEffect(() => {
+    const getDetails = async () => {
+      const res = await axios.get(`/api/v1/movie/${id}/similar`);
+      setSimilarContent(res.data.similars);
+    };
+    getDetails();
+  }, [id]);
   return (
     <div className="text-white bg-black min-h-screen">
       <div className="max-auto container px-4 py-8 h-full border-indigo-100 border-4">
         <Navbar />
         {/* Tailers Buttons */}
-        <div className="flex justify-between items-center mb-4">
-          <button className="rounded py-2 px-4 bg-gray-500/70 hover:bg-gray-500 bg-opacity-50 text-white">
+        <div className="flex justify-between items-center mb-4 mx-16">
+          <button
+            onClick={() => {
+              setTrailerIndex(trailerIndex - 1);
+            }}
+            className={`rounded py-2 px-4 bg-gray-500/70 hover:bg-gray-500 bg-opacity-50 text-white ${
+              trailerIndex === 0 ? "opacity-50 cursor-not-allowed " : ""
+            }`}
+          >
             <ChevronLeft size={24}></ChevronLeft>
           </button>
-          <button className="rounded py-2 px-4 bg-gray-500/70 hover:bg-gray-500 bg-opacity-50 text-white">
+          <button
+            onClick={() => {
+              setTrailerIndex(trailerIndex + 1);
+            }}
+            className={`rounded py-2 px-4 bg-gray-500/70 hover:bg-gray-500 bg-opacity-50 text-white ${
+              trailerIndex === trailers?.length - 1
+                ? "opacity-50 cursor-not-allowed "
+                : ""
+            }`}
+          >
             <ChevronRight size={24}></ChevronRight>
           </button>
         </div>
@@ -36,11 +94,11 @@ const WatchPage = () => {
             controls={true}
             width={"100%"}
             height={"70vh"}
-            url="https://www.youtube.com/watch?v=LXb3EKWsInQ"
+            url={`https://www.youtube.com/watch?v=${trailers?.[trailerIndex]?.key}`}
           />
         </div>
         {/* Content details*/}
-        <div className="flex md:flex-row flex-col items-center justify-between max-w-6xl gap-20 mx-auto">
+        <div className="flex md:flex-row my-10 flex-col items-center justify-between max-w-6xl gap-20 mx-auto">
           <div className="mb-4 md:mb-0">
             <h1 className="text-balance text-5xl font-bold">
               {details?.title}
@@ -65,6 +123,54 @@ const WatchPage = () => {
           />
         </div>
         {/* End Details */}
+        {/* Begin similar content */}
+        <div
+          className="relative px-5 mx-auto"
+          onMouseEnter={onMouseEnter}
+          onMouseLeave={onMouseLeave}
+        >
+          <h2 className="text-3xl mb-4 font-bold">You may also like</h2>
+          <div
+            className="flex space-x-4 overflow-x-scroll scrollbar-hide"
+            ref={sliderRef}
+          >
+            {similarContent?.map((content) => {
+              return (
+                <Link
+                  className="min-w-[250px]"
+                  to={`/watch/${content?.id}`}
+                  key={content?.id}
+                >
+                  <img
+                    className="rounded-lg"
+                    src={`${SMALL_IMG_BASE_URL}${content.poster_path}`}
+                    alt=""
+                  />
+                  <p className="mt-2 text-center">
+                    {content?.title || content?.name}
+                  </p>
+                </Link>
+              );
+            })}
+          </div>
+          {showArrows && (
+            <>
+              <button
+                onClick={scrollLeft}
+                className="absolute flex items-center justify-center size-12 top-1/2 left-5 md:left-10 rounded-full bg-black bg-opacity-50 hover:bg-opacity-75"
+              >
+                <ChevronLeft size={24}></ChevronLeft>
+              </button>
+              <button
+                onClick={scrollRight}
+                className="absolute flex items-center justify-center size-12 top-1/2 right-5 md:right-10 rounded-full bg-black bg-opacity-50  hover:bg-opacity-75"
+              >
+                <ChevronRight size={24}></ChevronRight>
+              </button>
+            </>
+          )}
+        </div>
+        {/* End similar content */}
       </div>
     </div>
   );
